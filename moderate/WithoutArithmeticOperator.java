f481548d90895a3b5396cea21c7744e810485fd4
imsaiful <saifulcseng@gmail.com>
2018-09-15 05:46:05 +0530

    add bit operation

---
+++ moderate/WithoutArithmeticOperator.java
@@ -0,0 +1,48 @@
+class WithoutArithmeticOperator{
+	public static void main(String[] args) {
+		int x=19,y=13;
+		Add_by_bit(x,y);
+		Sub_by_bit(x,y);
+		Mul_by_bit(x,y);
+	}
+
+
+	public static void Add_by_bit(int x,int y){
+		while(y!=0){
+			int carry=x&y;
+			System.out.println("carry="+carry);
+			x=x^y;
+			System.out.println("x="+carry);
+			y=carry<<1;
+		}
+		System.out.println(x);
+	}
+
+
+	public static void Sub_by_bit(int x,int y){
+		while(y!=0){
+			int borrow=(~x)&y;
+			x=x^y;
+			y=borrow<<1;
+		}
+		System.out.println(x);
+	}
+
+	public static void Mul_by_bit(int x,int y){
+		int result=0;
+		while(y!=0){
+			
+			if((y&1)!=0){
+				result+=x;
+			}
+			x=x<<1;
+			y=y>>1;
+		}
+		System.out.println(result);
+	}	
+
+
+
+
+
+}
