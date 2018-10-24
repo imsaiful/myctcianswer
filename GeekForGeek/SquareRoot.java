7c0e634a755cd81476d7ec68132abc2254c8086d
imsaiful <saifulcseng@gmail.com>
2018-10-25 01:29:34 +0530

    add

---
+++ GeekForGeek/SquareRoot.java
@@ -0,0 +1,29 @@
+class SquareRoot{
+	public static void main(String[] args) {
+		int x=81;
+		System.out.print(sqrt(x));
+	}
+	public static int sqrt(int x){
+		int start=0;
+		int end=x;
+		int ans=0;
+		while(start<=end){
+			int mid=(start+end)/2;
+			
+			if(mid*mid==x){
+				return mid;
+			}
+			else if(mid*mid<x){
+				start=mid+1;
+				ans=mid;
+			}
+			else{
+				end=mid;
+			}
+		}
+		return ans;
+
+
+	}
+
+}
