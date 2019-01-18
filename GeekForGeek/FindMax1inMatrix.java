2efa56da3f468aa9dd8fa0f7e596ba9c4df43f2a
imsaiful <saifulcseng@gmail.com>
2019-01-18 14:24:01 +0530

    add max 1 in matrix

---
+++ GeekForGeek/FindMax1inMatrix.java
@@ -0,0 +1,22 @@
+class FindMax1inMatrix{
+	public static void main(String[] args) {
+		int[][] m = { {0, 0, 0, 1}, 
+                       {0, 1, 1, 1}, 
+                       {1, 1, 1, 1}, 
+                       {0, 0, 0, 0}}; 
+  		int j=m[0].length-1;
+  		int row=0;
+  		int i;
+  		System.out.println(m.length+"  "+m[0].length);
+  		for(i=0;i<m.length;i++){
+  			while(j>=0 && m[i][j]==1){
+  				row=i;
+  				j--;
+  			}
+  		}
+  		System.out.print(row+1);
+
+
+
+	}
+}
---
+++ GeekForGeek/SwapNode.java
@@ -0,0 +1,41 @@
+class Node{
+		int data;
+		Node next;
+		Node(int d){
+			data=d;
+			next=null;
+		}
+	}
+class SwapNode{
+	static Node head;
+	public static void main(String[] args) {
+		SwapNode sn=new SwapNode();
+		sn.head=new Node(1);
+		sn.head.next=new Node(2);
+		sn.head.next.next=new Node(3);
+		sn.head.next.next.next=new Node(4);
+		sn.head.next.next.next.next=new Node(5);
+		sn.head.next.next.next.next.next=new Node(6);
+		sn.head.next.next.next.next.next.next=new Node(7);
+		sn.head.next.next.next.next.next.next.next=new Node(8);
+		sn.head.next.next.next.next.next.next.next.next=new Node(9);
+		help(head);
+		Print(head);
+	}
+	public static void help(Node head){
+		Node n=head;
+		while(n!=null && n.next!=null){
+			int temp=n.data;
+			n.data=n.next.data;
+			n.next.data=temp;
+			n=n.next.next;
+		}
+	}
+	public static void Print(Node head){
+		Node temp=head;
+		while(temp!=null){
+			System.out.print(temp.data+" ");
+			temp=temp.next;
+		}
+	}
+}
