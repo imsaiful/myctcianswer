b804d81f34d2814f0751045f9671dc146dc16495
imsaiful <saifulcseng@gmail.com>
2019-02-14 21:54:50 +0530

    add question

---
+++ GeekForGeek/NextGreatest.java
@@ -0,0 +1,18 @@
+/* Print Next Greatest */
+
+class NextGreatest{
+	public static void main(String[] args) {
+		int[] a= {11, 13, 21, 3};
+		int n=a.length;
+		for(int i=0;i<n;i++){
+			int next=-1;
+			for(int j=i+1;j<n;j++){
+				if(a[i]<a[j]){
+					next=a[j];
+					break;
+				}
+			}
+			System.out.print(next+" ");
+		}	
+	}
+}
