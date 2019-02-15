28a17025cbc6ef6e71158a24878de90c9a7d7ceb
imsaiful <saifulcseng@gmail.com>
2019-02-15 20:01:23 +0530

    amazon set

---
+++ GeekForGeek/IntToRoman.java
@@ -0,0 +1,31 @@
+/*Integer to roman*/
+
+
+
+class IntToRoman{
+	public static void main(String[] args) {
+		int n=3549;
+		System.out.println(help(n));
+	}
+	public static String help(int n){
+		String[] t={"","M","MM","MMM"};
+		String[] h={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+		String[] d={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+		String[] s={"","I","II","III","IV","V","VI","VII","VIII","IX"};
+
+		String n1=t[n/1000];
+		String n2=h[(n%1000)/100];
+		String n3=d[(n%100)/10];
+		String n4=s[(n%10)];
+		return n1+n2+n3+n4;
+
+
+	}
+
+
+
+
+
+
+
+}
---
+++ GeekForGeek/KMPAlgo.java
@@ -0,0 +1,59 @@
+/*KMP Algorithm*/
+
+class KMPAlgo{
+	public static void main(String[] args) {
+		String s="mynameissaifulhasan";
+		string p="saiful";
+		find(s,p);
+	}
+
+	public static void find(String s,String p){
+		int m=s.length();
+		int n=p.length();
+		int[] lps=computePattern(p);
+		int i=0;
+		int j=0;
+		while(i<m){
+			if(s.charAt(i)==s.charAt(j)){
+				i++;
+				j++;
+				if(j==n){
+					System.out.println("pattern matched at "+(i-j));
+					i=m;
+					break;
+				}
+			}
+			else{
+				if(j!=0){
+					j=lps[i-1];
+				}
+				else{
+					i++;
+				}
+			}
+		}
+	}
+	public static int[] computePattern(String p){
+		int n=p.length();
+		int[] lps=new int[n+1];
+		int i=1;
+		int j=0;
+		while(i<n){
+			if(p.charAt(i)==p.charAt(j)){
+				j++;
+				lps[i]=j;
+				i++;
+			}
+			else{
+				if(j!=0){
+					j=lps[i-1];
+				}
+				else{
+					lps[i]=0;
+					i++;
+				}
+			}
+		}
+		return lps;
+	} 
+}
